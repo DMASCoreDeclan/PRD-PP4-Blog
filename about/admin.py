dbc749f8b96d119985fed68a93fabfd8bf95b412
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-01-17 16:42:04 +0000

    Create the Collaboration Request form

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,4 +7,9 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
+
 # Register your models here.
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
