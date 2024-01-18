e9a3ae83d0562af8f636458816e62ae2f0271906
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-01-18 14:36:12 +0000

    Add EDIT button to post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 
 # Create your views here.
-
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
@@ -38,7 +37,7 @@ def post_detail(request, slug):
             comment.save()
             messages.add_message(
                 request, messages.SUCCESS, 
-                'Your comment has been submitted and will be available to see by everyone, once it\' approved by the Administrator'
+                'Your comment has been submitted and will be available to see by everyone, once it\'s approved by the Administrator'
             )
     comment_form = CommentForm()
     print("About to render template")
@@ -52,3 +51,23 @@ def post_detail(request, slug):
             'comment_form': comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form =CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = postcomment.approved = Falsecomment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment updated!')
+        else:
+            messages.add_message(request,message.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
