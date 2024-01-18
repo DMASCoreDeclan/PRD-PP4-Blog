dce8d689d3dd6e11e7a08400f99e5ebf248a6033
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-01-18 13:49:18 +0000

    Add POST functionality to CollaborateForm so that it posts to /admin

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -8,8 +9,21 @@ def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        print("Received a POST request from Collaborte section")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS, 
+                'Your collabortion request has been submitted to the Administrator'
+            )
+    print("About to render template")
+    
     about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+    collaborate_form = CollaborateForm()   
+    
+
 
     return render(
         request, "about/about.html", {
