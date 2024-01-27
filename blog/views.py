3aad71b209b9d5027701962a3cf155578056da91
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-01-27 10:38:38 +0000

    Formatting changes on post_detail and base.html

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,6 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -40,7 +39,6 @@ def post_detail(request, slug):
                 'Your comment has been submitted and will be available to see by everyone, once it\'s approved by the Administrator'
             )
     comment_form = CommentForm()
-    print("About to render template")
     return render(
         request, 
         'blog/post_detail.html', 
--- blogstar/settings.py
+++ blogstar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-dmascoredecl-prdpp4blog-2dthyin5zax.ws-eu107.gitpod.io',
