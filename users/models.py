bb40a686aade751d597549e4c5f8b81f4861c238
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-01-30 12:15:24 +0000

    Add profile_picture to profile.html so users can display a picture of themselves

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+'''
+This model allows a user to upload a profile picture to their profile
+'''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return f"{self.user.username}s Profile" 
