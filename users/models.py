78a6061203ac1d645c595d97227fae8c4c6b015c
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-02-14 10:36:15 +0000

    Minor changes to remove commented code before testing

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ from cloudinary.models import CloudinaryField
 
 '''
 This model allows a user to upload a profile picture to their profile
+inspired by Corey Schafer:
+https://www.youtube.com/watch?v=q4jPR-M0TAQ&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=6
 '''
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
